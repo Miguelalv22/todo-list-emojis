17ff43c3b979c34b84a95a607a4ed26692fb818f
Miguel Lopez <miguel.2211.lopez@gmail.com>
2025-07-25 17:45:51 -0600

    Se colocan punto y coma faltantes

--- src/TodoItem/TodoItem.jsx
+++ src/TodoItem/TodoItem.jsx
@@ -1,12 +1,12 @@
-import { useContext } from "react"
-import "./TodoItem.css"
-import { ShowPageContext } from "../context/Context"
+import { useContext } from "react";
+import "./TodoItem.css";
+import { ShowPageContext } from "../context/Context";
 
 export default function TodoItem({ id, todo, emoji }) {
     const { deleteTodo } = useContext(ShowPageContext);
 
     function handleClick() {
-        deleteTodo(id)
+        deleteTodo(id);
     }
 
     return (
